0184a166e5533caae4e1f023f785ff6a807bdd94
jerson100 <juamkoo@gmail.com>
2023-02-03 18:32:22 -0500

    git diff

--- src/components/commons/Header/header.style.ts
+++ src/components/commons/Header/header.style.ts
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const HeaderContainerStyle = styled.header`
-  /* border: solid 1px red; */
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -12,6 +11,11 @@ export const HeaderContainerStyle = styled.header`
   left: 0;
   width: 100%;
   z-index: 10000;
+  @media screen and (${(props) => props.theme.breakpoints.lg.media}) {
+    padding-left: 40px;
+    padding-right: 40px;
+    height: 80px;
+  }
 `;
 
 export const MenuBarContainerStyle = styled.div`
